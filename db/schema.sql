9c7d5c9623fc72fedc1a74f99b55dd0b004dfb5a
Mustafa Ali <mustafamire@gmail.com>
2021-09-11 21:05:04 +0100

    Add: Table role

--- db/schema.sql
+++ db/schema.sql
@@ -6,3 +6,13 @@ CREATE TABLE department (
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL
 );
+
+
+CREATE TABLE role (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL, 
+    salary DECIMAL NOT NULL,
+    department_id INTEGER, 
+    INDEX dep_ind (department_id),
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+);
