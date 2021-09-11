403da323c2f8ff631753bfe35b9b2d43959dc1d6
Mustafa Ali <mustafamire@gmail.com>
2021-09-11 21:27:54 +0100

    Add: Role seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,3 +4,16 @@ VALUES
 ('Finance & Accounting'),
 ('Sales & Marketing'),
 ('Operations');
+
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+('Full Stack Developer', 80000, 1),
+('Software Engineer', 120000, 1),
+('Accountant', 10000, 2), 
+('Finanical Analyst', 150000, 2),
+('Marketing Coordindator', 70000, 3), 
+('Sales Lead', 90000, 3),
+('Project Manager', 100000, 4),
+('Operations Manager', 90000, 4);
+
