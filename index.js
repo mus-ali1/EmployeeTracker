88f128e267dade6ef9dc10ebb925a55b991a7c61
Mustafa Ali <mustafamire@gmail.com>
2021-09-15 14:00:56 +0100

    Fix: connection to mysql + dotenv

--- index.js
+++ index.js
@@ -20,9 +20,10 @@ app.use(express.json());
 const connection = mysql.createConnection({
 
     host: "localhost",
-    user: "root",
-    password: "password",
-    database: "employee_db",
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+
 
 });
 
